fc28df357ccf6a2bb814b5094a0ed50a323d8abd
florianakos <florian.akos.szabo@gmail.com>
2018-06-07 15:24:39 +0300

    Trying to fix...

--- tools/travis-deploy.sh
+++ tools/travis-deploy.sh
@@ -68,7 +68,7 @@ set +x
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_KEY=../${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 #openssl aes-256-cbc -K $encrypted_dc3e238ed4e4_key -iv $encrypted_dc3e238ed4e4_iv -in ../deploy_key_cv.enc -out deploy_key_cv -d
 openssl aes-256-cbc -K $encrypted_b6a4c68c1fcb_key -iv $encrypted_b6a4c68c1fcb_iv -in deploy_key.enc -out ~\/.ssh/deploy_key -d
