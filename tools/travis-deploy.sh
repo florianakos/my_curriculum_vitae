32c503bad3ee7d3a817626b18de4e2e270b6f563
florianakos <florian.akos.szabo@gmail.com>
2018-06-07 15:56:11 +0300

    Fixing key path one last time

--- tools/travis-deploy.sh
+++ tools/travis-deploy.sh
@@ -77,9 +77,9 @@ if [ -n "$DEBUG" ] ; then
   set -x
 fi
 
-chmod 600 deploy_key_cv
+chmod 600 deploy_key
 eval `ssh-agent -s`
-ssh-add deploy_key_cv
+ssh-add deploy_key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
